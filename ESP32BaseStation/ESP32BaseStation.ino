ef2b42848525c11064c996acc16d44d7d0bf020e
jgOhYeah <koyugaDev@gmail.com>
2024-09-25 01:45:05 +1000

    Plotting ADC max and min in plot strain gauge, basic raw compensation in plotter.

--- ESP32BaseStation/ESP32BaseStation.ino
+++ ESP32BaseStation/ESP32BaseStation.ino
@@ -55,7 +55,7 @@ void setup()
 
     Serial.begin(SERIAL_BAUD); // Already running from the bootloader.
     Serial.setDebugOutput(true);
-    LOGI("Setup", "Farm PJON LoRa base station v" VERSION ". Compiled " __DATE__ ", " __TIME__);
+    LOGI("Setup", "Farm PJON LoRa base station v" SW_VERSION ". Compiled " __DATE__ ", " __TIME__);
 
     if (!alarmQueue || !audioQueue || !mqttPublishQueue || !mqttMutex ||
         !serialMutex || !loraMutex || !stateUpdateMutex)
